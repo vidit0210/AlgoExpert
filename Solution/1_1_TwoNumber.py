8a42db536fc7159eb4a80437a3194f0d5a51a1ab
vidit0210 <0210vidit@gmail.com>
2019-06-04 00:02:54 +0530

    1_Python

---
+++ Solution/1_1_TwoNumber.py
@@ -0,0 +1,7 @@
+def twoNumberSum(array, targetSum):
+    pass
+
+
+print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10))
+print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 11))
+print(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 99))
